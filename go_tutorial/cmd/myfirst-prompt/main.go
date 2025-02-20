04d472514af0dafbc6da2dbbf50b88dbb55a35b0
Christopher Novak <cnovak@redhat.com>
2025-02-20 08:55:46 -0500

    Change secure print

--- go_tutorial/cmd/myfirst-prompt/main.go
+++ go_tutorial/cmd/myfirst-prompt/main.go
@@ -34,5 +34,5 @@ func main() {
 	fmt.Println("You entered: ", result)
 
 	var secureresult string = readUserInputSecretly()
-	fmt.Println("You entered: ", secureresult)
+	fmt.Println("You securely entered: ", secureresult)
 }
